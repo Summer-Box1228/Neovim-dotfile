796938788408f450f45973d98f9e02ee2e9879d9
summer <tingrong1228@gmail.com>
2025-11-21 16:15:40 +0800

    init commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotag = { enable = true },
+	    ensure_installed = {
+		"lua",
+		"java",
+		"cpp",
+		"c",
+	    },
+	    auto_install = false,
+	})
+    end
+}
